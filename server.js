c516c8f8fb8a8f39dce9221a69f2f1601b5d86d2
ali-aloula <abdelaliazzeddine29@gmail.com>
2025-04-26 11:15:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
-
 let waitingUser = null;
 
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    if (waitingUser) {
-        // Pair with waiting user
-        socket.emit('match', waitingUser.id);
-        waitingUser.emit('match', socket.id);
-        waitingUser = null;
-    } else {
-        // No one waiting, store current user
-        waitingUser = socket;
-    }
+    socket.on('ready', () => {
+        if (waitingUser && waitingUser !== socket) {
+            // Match them
+            socket.emit('match', waitingUser.id);
+            waitingUser.emit('match', socket.id);
+            waitingUser = null;
+        } else {
+            // No one waiting
+            waitingUser = socket;
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
         if (waitingUser === socket) {
             waitingUser = null;
         }
+        socket.broadcast.emit('partner-disconnected');
     });
-});
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('ice-candidate', (candidate) => {
+        socket.broadcast.emit('ice-candidate', candidate);
+    });
 });
