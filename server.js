0afdeb5caf983c08cd22a56eb89be17090c27b07
ali-aloula <abdelaliazzeddine29@gmail.com>
2025-04-26 11:29:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
-// Array to keep track of online users (just for matching purposes)
+// Create an express app
+const app = express();
+
+// Create an HTTP server to work with Socket.IO
+const server = http.createServer(app);
+
+// Create a Socket.IO instance attached to the HTTP server
+const io = socketIo(server, {
+    cors: {
+        origin: "https://wow.buy-scripts.com", // Allow your frontend domain
+        methods: ["GET", "POST"], // Allow GET and POST methods
+        allowedHeaders: ["Content-Type"], // Allow Content-Type header
+        credentials: true, // Allow cookies to be sent across origins
+    },
+});
+
 let users = [];
 
-io.on('connection', socket => {
-    console.log('New connection:', socket.id);
+io.on('connection', (socket) => {
+    console.log(`New connection: ${socket.id}`);
 
     socket.on('ready', () => {
-        console.log(socket.id, 'is ready');
+        console.log(`${socket.id} is ready`);
         users.push(socket.id);
 
-        // If there are at least 2 users, match them
+        // Check if there are two users ready, then match them
         if (users.length >= 2) {
-            const user1 = users.pop();
-            const user2 = users.pop();
+            const user1 = users.pop(); // First user in the queue
+            const user2 = users.pop(); // Second user in the queue
+            console.log('Matched users:', user1, user2);
+
+            // Emit the "match" event to both users
             io.to(user1).emit('match', user2);
             io.to(user2).emit('match', user1);
         }
     });
 
-    socket.on('offer', (offer, partnerId) => {
-        io.to(partnerId).emit('offer', offer);
-    });
-
-    socket.on('answer', (answer, partnerId) => {
-        io.to(partnerId).emit('answer', answer);
-    });
-
-    socket.on('ice-candidate', (candidate, partnerId) => {
-        io.to(partnerId).emit('ice-candidate', candidate);
-    });
-
     socket.on('disconnect', () => {
-        console.log(socket.id, 'disconnected');
-        users = users.filter(id => id !== socket.id);
-        io.emit('partner-disconnected');
+        console.log(`${socket.id} disconnected`);
+        users = users.filter(id => id !== socket.id);  // Remove disconnected user
+        io.emit('partner-disconnected');  // Notify all clients
     });
 });
 
+// Start the server
 server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log('Server listening on port 3000');
 });
